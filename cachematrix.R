9e80b0eaa49e29b1fc46cf40d1205e7ed58e7611
amit-d <amitongit@gmail.com>
2014-05-24 23:11:41 -0400

    added code for computation of inverse matrix to stub file

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,47 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix and cacheSolve functions in conjuction help to avoid
+## repeating computation of inverse of a matrix since such as computations
+## can take long time, especially for large matrices.
 
-## Write a short comment describing this function
+## Comments adopted from Good explanation by James A. Stephenson (Community TA)
+##at link-
+## https://class.coursera.org/rprog-003/forum/thread?thread_id=664
+
+## Test cases provided by Gregory D. Horne (Community TA) used from link-
+## https://class.coursera.org/rprog-003/forum/thread?thread_id=650
+
+
+
+## makeCacheMatrix function-
+## Creates a list of subfunctions to manage cache for computation 
+## of inverse of matrix.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        I <- NULL
+        set <- function(y){
+                x <<- y
+                I <<- NULL
+        }
+        get <- function() x
+        savecache <- function(solve) I <<- solve
+        getcache <- function() I
+        list(set=set, get=get, savecache=savecache, getcache=getcache)
 }
 
 
-## Write a short comment describing this function
+
+## cacheSolve function- 
+## computes inverse of matrix only if cache does not already contain 
+##computation from previous function call for the same matrix
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        I <- x$getcache()
+        if(!is.null(I)){
+                message("getting cached data")
+                return(I)
+        }
+        data <- x$get()
+        I <- solve(data, ...)
+        x$savecache(I)
+        I
 }
